92dc7c1125e39ea87e612e58ca0bf6941ec5bc73
Kate Welch <kwelch95@gmail.com>
2020-12-04 21:42:54 -0800

    Working

---
+++ Dec1/app.py
@@ -0,0 +1,28 @@
+from itertools import combinations
+
+FILENAME = "input.txt"
+
+
+with open(FILENAME,"r") as f:
+    data = f.read()
+
+#print(repr(data))
+
+numbers = []
+
+for line in data.splitlines():
+    #print(repr(line))
+    number = int(line)
+    numbers.append(number)
+
+#print(numbers)
+
+combos = combinations(numbers,2)
+#print(list(combos))
+
+for x, y in combos:
+    sums = x + y
+    if sums == 2020:
+        print(x, y, x * y)
+        break
+
